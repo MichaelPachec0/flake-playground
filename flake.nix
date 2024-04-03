f417769370ca07e055fd0ef096978be6fc858264
Michael Pacheco <git@michaelpacheco.org>
2024-04-03 01:04:35 -0700

    flake: add overlay and rework default.
    * removed defaults, this flake will probably include more packages and
      packages will change as I keep working on packaging more packages.
    * include an overlay, this way it can be brought in as a flake input, and
      used in a flake based config, making it easier to be installed as part
      of a system config.
    * NOTE: I am using a playground namespace so to mark that these packages
      as test packages. Might decide later whether to keep this nomenclature
      or move to overlay without a clear seperation of packages.
    * Remove default.nix, this was not supposed to be commited and this was
      a parallel test being conducted to see how much cleanup was needed.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- flake.nix
+++ flake.nix
@@ -3,29 +3,33 @@
     nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    system = "x86_64-linux";
-    prepNixpkgs = _nixpkgs: system:
-      import _nixpkgs {
-        config.allowUnfree = true;
-        inherit system;
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      inherit (self) outputs;
+      # NOTE: for now this has only been tested with x86_64-linux.
+      # TODO: adapt this a better flake style, there better written flakes out there.
+      # For now though this works.
+      system = "x86_64-linux";
+      prepNixpkgs = _nixpkgs: system:
+        import _nixpkgs {
+          config.allowUnfree = true;
+          inherit system;
+        };
+      pkgs = prepNixpkgs nixpkgs system;
+      linux-show-player = pkgs.callPackage ./nix/linux-show-player.nix { };
+    in {
+      packages = { x86_64-linux = { inherit linux-show-player; }; };
+      overlays = let
+        playground = final: prev: {
+          playground = { inherit linux-show-player; };
+        };
+      in {
+        inherit playground;
+        default = playground;
       };
-    pkgs = prepNixpkgs nixpkgs system;
-    test-build = pkgs.callPackage ./nix/linux-show-player.nix {};
-  in {
-    packages = {
-      # x86_64-linux.default = linux-show-player;
-      x86_64-linux.default = test-build;
+      devShells.x86_64-linux = {
+        linux-show-player =
+          pkgs.mkShell { packages = with pkgs; [ linux-show-player ]; };
+      };
     };
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = with pkgs; [
-        test-build
-      ];
-    };
-  };
 }
