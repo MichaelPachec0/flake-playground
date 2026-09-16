9563e3ef99375f6b9457cff693c525758a3cf349
MichaelPachec0 <48970112+MichaelPachec0@users.noreply.github.com>
2026-09-16 09:40:31 +0000

    chore: bump playground package sources

--- nix/pkgs/playground/_sources/generated.nix
+++ nix/pkgs/playground/_sources/generated.nix
@@ -66,41 +66,41 @@
   };
   freebuff-darwin-arm64 = {
     pname = "freebuff-darwin-arm64";
-    version = "0.0.174";
+    version = "0.0.175";
     src = fetchurl {
-      url = "https://github.com/CodebuffAI/codebuff-community/releases/download/freebuff-v0.0.174/freebuff-darwin-arm64.tar.gz";
-      sha256 = "sha256-uq3dMDBIVb6c5spD3bzoiK3WH7+zpwBiOIFKxuKEK/Y=";
+      url = "https://github.com/CodebuffAI/codebuff-community/releases/download/freebuff-v0.0.175/freebuff-darwin-arm64.tar.gz";
+      sha256 = "sha256-5QDkvSoAvcPXh7TfeGp4bl8M+Suw29qfXdr9lMZ86QQ=";
     };
   };
   freebuff-linux-arm64 = {
     pname = "freebuff-linux-arm64";
-    version = "0.0.174";
+    version = "0.0.175";
     src = fetchurl {
-      url = "https://github.com/CodebuffAI/codebuff-community/releases/download/freebuff-v0.0.174/freebuff-linux-arm64.tar.gz";
-      sha256 = "sha256-GJ3SZ1DDVfdlulODSXchtj0A/Qste7f8Bk8HX9NEyHQ=";
+      url = "https://github.com/CodebuffAI/codebuff-community/releases/download/freebuff-v0.0.175/freebuff-linux-arm64.tar.gz";
+      sha256 = "sha256-71PqLxp+2k28tCs7au+bK8OT4xZM2Sy8AFXnJkAAV2s=";
     };
   };
   freebuff-linux-x64 = {
     pname = "freebuff-linux-x64";
-    version = "0.0.174";
+    version = "0.0.175";
     src = fetchurl {
-      url = "https://github.com/CodebuffAI/codebuff-community/releases/download/freebuff-v0.0.174/freebuff-linux-x64.tar.gz";
-      sha256 = "sha256-W/+lPhdHxq0a5TWY/JodAuP2hLpCGlLU7E8SoNe+OYA=";
+      url = "https://github.com/CodebuffAI/codebuff-community/releases/download/freebuff-v0.0.175/freebuff-linux-x64.tar.gz";
+      sha256 = "sha256-OjiWDnkaJi+GrxaB9G+aQP43ml/6nGUUSjqOSTWQWjQ=";
     };
   };
   powertop = {
     pname = "powertop";
-    version = "87b8ea9f5a34450cd3954a40e0d5343c11d33893";
+    version = "063862e5ca88687d8f393a35ed562f7cc10ffc46";
     src = fetchgit {
       url = "https://github.com/fenrus75/powertop";
-      rev = "87b8ea9f5a34450cd3954a40e0d5343c11d33893";
+      rev = "063862e5ca88687d8f393a35ed562f7cc10ffc46";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
       sparseCheckout = [ ];
-      sha256 = "sha256-VtIZFMslICSY4lJvlsqt+8IXavKBiV0VglWl8V+ZA3I=";
+      sha256 = "sha256-3hVJe/JJLafbPYXtpG5ApQhL7PkzbYHX12IEgUzrrNI=";
     };
-    date = "2026-09-09";
+    date = "2026-09-15";
   };
   workstyle = {
     pname = "workstyle";
